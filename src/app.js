3545961f2b09da4c80db299fe70ee855fe65e446
RamzyMerad <ramzy.merad@student.ehb.be>
2021-11-11 22:00:12 +0100

    endpoints Get/Post/update/delete

--- src/app.js
+++ src/app.js
@@ -14,4 +14,58 @@ const users = [];
 
 app.use(express.json());
 
+app.get("/", async(req, res) => {
+  await pg.select().from('animes')
+  .then(data => {
+    res.send(data)
+  })
+
+})
+app.put('/', async (req, res) => {
+  const {title: title, studio: studio, episodes: episodes, image: image} = req.body
+  if(title,  studio, episodes,  image){
+      await pg('animes')
+      .where('title', title)
+      .update({
+        title: title,
+        studio: studio,
+        episodes: episodes,
+      })
+      .then(data => {
+        if(data !== 1) return res.sendStatus(400)
+        res.sendStatus(200)
+      })
+  }else{
+    res.sendStatus(400);
+  }
+})
+
+app.post("/",async(req,res)=>{
+  const {title: title, studio: studio, episodes: episodes, image: image} = req.body
+  if(title,  studio, episodes,  image){
+
+        await pg('animes').insert({title: title, studio: studio, episodes: episodes, image: image})
+        .then(data => {
+          res.sendStatus(200);
+        })
+    
+  }else{
+    res.sendStatus(400);
+  }
+});
+
+
+
+app.delete('/', async (req, res) => {
+  const {id} = req.body
+  console.log(id)
+  if(!id) return res.sendStatus(400);
+  console.log(id)
+  await pg('animes').where('id', id)
+  .del()
+  .then(data => {
+    if(data !== 1) return res.sendStatus(400)
+    res.sendStatus(200)
+  })
+})
 
